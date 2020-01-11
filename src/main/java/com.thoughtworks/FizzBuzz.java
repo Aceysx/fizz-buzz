cf2dd3fa5e65172e7295a4a2e12a7de54de68ab9
aceysx <aceysx@163.com>
2020-01-11 10:59:49 +0800

    return number not multiples of 3 or 5 or 7

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -2,4 +2,7 @@ package com.thoughtworks;
 
 public class FizzBuzz {
 
+    public static String of(Integer number) {
+        return String.valueOf(number);
+    }
 }
--- src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -1,5 +1,16 @@
 package com.thoughtworks;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FizzBuzzTest {
+
+    @Test
+    public void should_return_number_when_not_multiples_of_3_or_5_or_7() {
+        assertEquals("1", FizzBuzz.of(1));
+        assertEquals("2", FizzBuzz.of(2));
+        assertEquals("4", FizzBuzz.of(4));
+    }
 
 }
