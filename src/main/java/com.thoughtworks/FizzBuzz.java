c3056fe67763aaf4cd1eedf0cb8e092c44dc8a5c
aceysx <aceysx@163.com>
2020-01-11 11:01:59 +0800

    return Fizz/Buzz/Whizz when multiples of 3 or 5 or 7

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -2,7 +2,24 @@ package com.thoughtworks;
 
 public class FizzBuzz {
 
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+    public static final String WHIZZ = "Whizz";
+
     public static String of(Integer number) {
-        return String.valueOf(number);
+        String result = "";
+        if (number % 3 == 0) {
+            result += FIZZ;
+        }
+        if (number % 5 == 0) {
+            result += BUZZ;
+        }
+        if (number % 7 == 0) {
+            result += WHIZZ;
+        }
+
+        return result.isEmpty()
+            ? String.valueOf(number)
+            : result;
     }
 }
--- src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -13,4 +13,23 @@ public class FizzBuzzTest {
         assertEquals("4", FizzBuzz.of(4));
     }
 
+    @Test
+    public void should_return_Fizz_when_multiples_of_3() {
+        assertEquals("Fizz", FizzBuzz.of(3));
+        assertEquals("Fizz", FizzBuzz.of(6));
+    }
+
+    @Test
+    public void should_return_Buzz_when_multiples_of_5() {
+        assertEquals("Buzz", FizzBuzz.of(5));
+        assertEquals("Buzz", FizzBuzz.of(10));
+    }
+
+    @Test
+    public void should_return_Whizz_when_multiples_of_7() {
+        assertEquals("Whizz", FizzBuzz.of(7));
+        assertEquals("Whizz", FizzBuzz.of(14));
+    }
+
+
 }
